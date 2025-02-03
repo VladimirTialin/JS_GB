1f9af68b51544c0c137bf95e36018dcb4c3feb63
Vladimir <wel.tall@yandex.ru>
2025-02-03 20:30:38 +0300

    work1 complite

---
+++ Homework/Homework1/script.js
@@ -0,0 +1,11 @@
+//Задание №1
+let temperature = 34;
+let fahrenheit = (9 / 5 * temperature) + 32;
+console.log(`Температура:
+по Цельсию: ${temperature} ℃,
+по Фаренгейту: ${fahrenheit} F`);
+
+//Задание №2
+let userName="Владимир",
+    adminName=userName;
+console.log(`Admin: ${adminName}`);
---
+++ Seminare/Seminare1/script.js
@@ -0,0 +1,54 @@
+//Задание №1
+console.log("Текст из script.js");
+
+//Задание №2
+let num = 123;
+console.log(`Задание 2.1: ${num}`);
+
+let a = 5;
+a = 7;
+console.log(`Задание 2.2: ${a}`);
+let b = 1 + 2 + 3;
+console.log(`Задание 2.3: ${b}`);
+
+/*Задания выполнены
+
+*/
+
+//Задание 3
+let c = 10, d = 2;
+let sum, diff, prod, div;
+
+sum=c+d;
+diff=c-d;
+prod=c*d;
+div=c/d;
+console.log(`Сумма = ${sum}, 
+Разность = ${diff}, 
+Произведение = ${prod}, 
+Деление = ${div}`);
+
+console.log(sum+diff+prod+div);
+
+//Задание 5
+let numberA=1.5;
+let nubmerB=0.75;
+
+console.log(numberA+nubmerB);
+
+let number=-45;
+
+console.log(-number);
+
+let firstName="Владимир";
+let lastName="Тялин";
+console.log(firstName+` `+lastName);
+
+let java = 'java', 
+    script ='script';
+
+console.log(java+script);
+
+let hello='Hello',
+    world='world';
+console.log(`${hello} ${world}!`);
