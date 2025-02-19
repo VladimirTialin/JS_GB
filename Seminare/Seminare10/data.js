4a15a60ec806895723c52dbd0f35d6d37669825e
Vladimir <wel.tall@yandex.ru>
2025-02-19 21:08:16 +0300

    seminare 10

---
+++ Seminare/Seminare10/data.js
@@ -0,0 +1,22 @@
+const dataInfo=`
+[
+    {
+        "id": "ef6",
+        "url": "https://cdn2.thecatapi.com/images/ef6.jpg",
+        "width": 500,
+        "height": 334
+    },
+    {
+        "id": "bpj",
+        "url": "https://cdn2.thecatapi.com/images/bpj.jpg",
+        "width": 769,
+        "height": 1061
+    },
+    {
+        "id": "dt7",
+        "url": "https://cdn2.thecatapi.com/images/dt7.png",
+        "width": 765,
+        "height": 1024
+    }
+]
+`
---
+++ Seminare/Seminare10/script.js
@@ -0,0 +1,7 @@
+import dataInfo from './data.js';
+let data=JSON.parse(dataInfo);
+
+data.forEach(element => {
+    console.log(element);
+});
+    
