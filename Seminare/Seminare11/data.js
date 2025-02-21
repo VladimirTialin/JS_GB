3406a04f0cae8ae294c4aaf92ab202dc596c0774
Vladimir <wel.tall@yandex.ru>
2025-02-21 17:04:47 +0300

    work11

---
+++ Seminare/Seminare11/data.js
@@ -0,0 +1,20 @@
+const dataJSON = ` 
+[
+    {
+        "image": "content2.jpg",
+        "title": "MANGO  PEOPLE T-SHIRT",
+        "price": 300,
+        "color": "Red",
+        "size": "XI",
+        "quantity": 2 
+    },
+    {
+        "image": "content1.jpg",
+        "title": "ELLERY X M'O CAPSULE",
+        "price": 52,
+        "color": "Black",
+        "size": "XI",
+        "quantity": 1 
+    }
+]`;
+const dataProducts=JSON.parse(dataJSON);
